607c215980b1126740c7fa044a36feb4119c061c
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-22 13:37:42 +0600

    feat: add metadata for seo

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,9 +1,67 @@
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const TITLE = "Furniro - Full stack e-commerce project.";
+const DESCRIPTION =
+  "Furniro is a full‑stack e‑commerce web application built with modern web technologies. It supports user authentication, product browsing with advanced filtering, cart and order management, and an admin dashboard for managing products and orders.";
+const NAME = "Md Shakerullah Sourov";
+
 export const metadata = {
-  title: "Furniro - No.1 furniture store in the USA",
-  description: "No.1 furniture store in the USA",
+  metadataBase: new URL(BASE_URL),
+
+  title: TITLE,
+  description: DESCRIPTION,
+
+  alternates: {
+    canonical: BASE_URL,
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+
+  // openGraph: {
+  //   type: "website",
+  //   url: BASE_URL,
+  //   title: TITLE,
+  //   description: DESCRIPTION,
+  //   siteName: TITLE,
+  //   images: [
+  //     {
+  //       url: "/og-image.png",
+  //       width: 1200,
+  //       height: 630,
+  //       alt: TITLE,
+  //     },
+  //   ],
+  // },
+
+  // twitter: {
+  //   card: "summary_large_image",
+  //   creator: "@mdshakerullahS",
+  //   images: ["/og-image.png"],
+  //   description: DESCRIPTION,
+  //   title: TITLE,
+  // },
+
+  icons: {
+    icon: "/favicon.ico",
+    // apple: "/apple-touch-icon.png",
+  },
+
+  creator: NAME,
+
+  publisher: NAME,
+};
+
+export const viewport = {
+  themeColor: "#b88e2f",
 };
 
 export default function RootLayout({ children }) {
