c71eadad284263aca3666ef9fcc0bffb4b812f4d
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-23 00:17:55 +0600

    update: decrease background opacity and use tailwind class for text color

--- frontend/src/app/(client)/page.js
+++ frontend/src/app/(client)/page.js
@@ -1,15 +1,17 @@
+import BrandStory from "@/components/BrandStory";
 import Categories from "@/components/Categories";
 import Hero from "@/components/Hero";
 import ProductsSection from "@/components/ProductsSection";
-import TrustStript from "@/components/TrustStript";
+import TrustStrip from "@/components/TrustStrip";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <TrustStript />
+      <TrustStrip />
       <Categories />
       <ProductsSection />
+      <BrandStory />
     </main>
   );
 }
