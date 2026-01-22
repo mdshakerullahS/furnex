78e2ba6125b2e80cd5c5defb267d61b0b465611d
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-22 22:22:17 +0600

    feat: create and add truststripe section

--- frontend/src/app/(client)/page.js
+++ frontend/src/app/(client)/page.js
@@ -1,11 +1,13 @@
 import Categories from "@/components/Categories";
 import Hero from "@/components/Hero";
 import ProductsSection from "@/components/ProductsSection";
+import TrustStript from "@/components/TrustStript";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <TrustStript />
       <Categories />
       <ProductsSection />
     </main>
