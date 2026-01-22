9b0d9a4eedf823478e543b53ddab265131516041
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-22 20:27:16 +0600

    update: update hero section layout and add carousel

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,13 +1,19 @@
 "use client";
 
-import heroImage1 from "../../public/Hero_Image_1.jpg";
-import heroImage2 from "../../public/Hero_Image_2.jpg";
-import heroImage3 from "../../public/Hero_Image_3.jpg";
-
 import Image from "next/image";
 import { AspectRatio } from "./ui/aspect-ratio";
 import { useEffect, useState } from "react";
 import SearchForm from "./SearchForm";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
+const images = new Array(3).fill(0);
 
 const Hero = () => {
   const [width, setWidth] = useState(0);
@@ -31,41 +37,33 @@ const Hero = () => {
       <div className="py-2 md:hidden">
         <SearchForm />
       </div>
-      <div className="flex flex-col lg:flex-row gap-2 md:gap-4 lg:gap-6">
-        <AspectRatio ratio={width <= 960 ? 2 : 2.85}>
-          <Image
-            src={heroImage2}
-            fill
-            sizes="75vw"
-            alt="Hero Background"
-            priority
-            className="h-[400px] rounded-2xl object-cover"
-          />
-        </AspectRatio>
-
-        <div className="w-full lg:w-1/2 flex lg:flex-col items-center justify-between gap-2 md:gap-4 lg:gap-0">
-          <AspectRatio ratio={width <= 960 ? 2.975 : 1.975}>
-            <Image
-              src={heroImage1}
-              fill
-              sizes="25vw"
-              alt="Hero Background"
-              priority
-              className="rounded-xl object-cover"
-            />
-          </AspectRatio>
-          <AspectRatio ratio={width <= 960 ? 2.975 : 1.975}>
-            <Image
-              src={heroImage3}
-              fill
-              sizes="25vw"
-              alt="Hero Background"
-              priority
-              className="rounded-xl object-cover"
-            />
-          </AspectRatio>
-        </div>
-      </div>
+      <AspectRatio ratio={width <= 960 ? 2 : 2.2}>
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+          className="h-full"
+        >
+          <CarouselContent className="h-full">
+            {images.map((_, idx) => (
+              <CarouselItem key={idx} className="relative">
+                <Image
+                  src={`/Hero_Image_${idx + 1}.jpg`}
+                  fill
+                  sizes="100vw"
+                  alt="Hero Background"
+                  priority
+                  className="h-[400px] rounded-2xl object-cover"
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </AspectRatio>
     </section>
   );
 };
