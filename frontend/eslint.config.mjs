56dda97f9f300c02b1fde04a7df08d263f0f6795
Shakerullah <sourovmdshakerullah@gmail.com>
2026-03-30 17:03:40 +0600

    feat: setup eslint and prettier

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,25 +1,67 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import globals from "globals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import prettierPlugin from "eslint-plugin-prettier";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import { defineConfig } from "eslint/config";
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+export default defineConfig([
+  js.configs.recommended,
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+    files: ["**/*.{js,jsx,mjs,cjs}"],
+    ignores: ["node_modules/**", ".next/**", "public/**", "dist/**"],
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      globals: { ...globals.node, ...globals.jest, ...globals.browser },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "react/jsx-uses-vars": "error",
+      "no-undef": "error",
+      "no-unreachable": "error",
+      "no-constant-condition": "warn",
+      "no-debugger": "error",
 
-export default eslintConfig;
+      eqeqeq: ["error", "always"],
+      "no-var": "error",
+      "prefer-const": "error",
+      "no-useless-return": "error",
+      "no-empty": ["error", { allowEmptyCatch: true }],
+      "no-shadow": "error",
+
+      "no-process-exit": "warn",
+      "no-sync": "warn",
+
+      semi: "off",
+      quotes: "off",
+      indent: "off",
+      "comma-dangle": "off",
+      "object-curly-spacing": "off",
+      "arrow-spacing": "off",
+      "prettier/prettier": "error",
+
+      "no-console": "warn",
+      "no-trailing-spaces": "error",
+      "eol-last": ["error", "always"],
+      "max-lines": ["warn", 300],
+      "max-depth": ["warn", 4],
+
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+]);
